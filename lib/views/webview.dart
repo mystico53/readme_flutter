d376502d254fc5c1254592ea9f9a2f74141df09c
mystico53 <konkaiser@gmail.com>
2024-04-10 23:35:06 -0700

    scrape html

--- lib/views/webview.dart
+++ lib/views/webview.dart
@@ -12,6 +12,7 @@ class WebViewPage extends StatefulWidget {
 
 class _WebViewPageState extends State<WebViewPage> {
   late final WebViewController _controller;
+  String _textContent = '';
 
   @override
   void initState() {
@@ -35,31 +36,31 @@ class _WebViewPageState extends State<WebViewPage> {
         ),
       )
       ..setUserAgent(
-          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36');
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+      );
   }
 
   void _scrapeTextContent() async {
     try {
       final String textContent =
           await _controller.runJavaScriptReturningResult('''
-      (function() {
-        try {
-          var text = "";
-          var elements = document.getElementsByTagName("*");
-          for (var i = 0; i < elements.length; i++) {
-            var element = elements[i];
-            if (element.innerText) {
-              text += element.innerText + "\\n";
+        (function() {
+          try {
+            var paragraphs = document.querySelectorAll('p');
+            var text = "";
+            for (var i = 0; i < paragraphs.length; i++) {
+              text += paragraphs[i].innerText + "\\n\\n";
             }
+            return text;
+          } catch (error) {
+            console.error("JavaScript error:", error);
+            return "";
           }
-          return text;
-        } catch (error) {
-          console.error("JavaScript error:", error);
-          return "";
-        }
-      })();
-    ''') as String;
-
+        })();
+      ''') as String;
+      setState(() {
+        _textContent = textContent;
+      });
       print('Text Content:');
       print(textContent);
     } catch (error) {
@@ -73,8 +74,23 @@ class _WebViewPageState extends State<WebViewPage> {
       appBar: AppBar(
         title: const Text('WebView'),
       ),
-      body: WebViewWidget(
-        controller: _controller,
+      body: Column(
+        children: [
+          Expanded(
+            child: WebViewWidget(
+              controller: _controller,
+            ),
+          ),
+          if (_textContent.isNotEmpty)
+            Expanded(
+              child: SingleChildScrollView(
+                child: SelectableText(
+                  _textContent,
+                  style: TextStyle(fontSize: 12),
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
